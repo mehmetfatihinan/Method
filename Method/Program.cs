a711eb405987cba4af1a786b73e01e4ccf2f71b1
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-29 00:55:45 +0300

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lyrics();
+
+        }
+
+        public static void Lyrics()
+        {
+            Console.WriteLine("A broken spirit that may never heal \r\n Try my best to erase all the memories");
+        }
+    }
+}
